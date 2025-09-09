8c4f8a20ec91a1c81e6b42e695fa0be425c30c21
armaan-hike <armaan.g@hike.in>
2025-09-09 11:10:51 +0530

    testing github connection

---
+++ src/main/java/org/hike/Main.java
@@ -0,0 +1,18 @@
+package org.hike;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Singleton single = Singleton.getSingleton();
+
+        System.out.println(single.hashCode());
+
+        Singleton single2 = Singleton.getSingleton();
+
+        System.out.println(single2.hashCode());
+
+
+    }
+}
---
+++ src/main/java/org/hike/Singleton.java
@@ -0,0 +1,42 @@
+package org.hike;
+
+public class Singleton {
+
+    // This is the eager way of creating singleton
+    private static Singleton singleton = new Singleton();
+
+    static Singleton getSingleton(){
+        return singleton;
+    }
+
+ /*
+
+    private static Singleton singleton;
+
+    //Constructor
+    private Singleton(){}
+
+
+    // This is lazy initialization, object will be created when client asks for
+    // not good for multi thread options as thread safety would be compromised
+    // we can use method syncronized keyword for this as T1 would use, T2 would wait and then execute
+    // better practice would be to use the block syncronization
+    public static Singleton getSingleton(){
+
+        //Object of this class
+        if(singleton == null){
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+
+  */
+}
+/*
+* 1. Keep the constructor private
+*
+* 2. object creation with the help of method
+*
+* 3. create field to store the object be private
+*
+* */
